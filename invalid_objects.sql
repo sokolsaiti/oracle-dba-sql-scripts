4a2c12eb9db58320a66d8a5a16a66e87b477ecf6
Sokol Saiti <3602050+sokolsaiti@users.noreply.github.com>
2019-01-29 10:31:45 +0100

    Create invalid_objects.sql

---
+++ invalid_objects.sql
@@ -0,0 +1,18 @@
+--Lists the invalid objects
+
+SET LINESIZE 500
+SET PAGESIZE 1000
+
+COLUMN owner FORMAT A15
+COLUMN object_type FORMAT A15
+COLUMN object_name FORMAT A50
+COLUMN status FORMAT A10
+
+
+SELECT owner,
+       object_type,
+       object_name,
+       status
+FROM   dba_objects
+WHERE  status = 'INVALID'
+ORDER BY owner, object_type, object_name;
