4d3c89bde45c41dcc064450994eb689fe668d472
Sokol Saiti <3602050+sokolsaiti@users.noreply.github.com>
2019-01-30 11:41:55 +0100

    Create vcontrolfile_records.sql

---
+++ vcontrolfile_records.sql
@@ -0,0 +1,12 @@
+SET LINESIZE 500
+SET PAGESIZE 1000
+
+COLUMN type FORMAT A30
+
+select r.type,
+       r.record_size,
+       r.records_total,
+       r.records_used 
+from v$controlfile_record_section r
+where r.records_used > 0
+order by r.records_total desc; 
