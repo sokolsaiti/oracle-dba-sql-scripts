40df9d1b61aefafb411a3a8326252ef8af50ae58
Sokol Saiti <3602050+sokolsaiti@users.noreply.github.com>
2019-01-30 10:48:27 +0100

    Create tablespaces.sql

---
+++ tablespaces.sql
@@ -0,0 +1,11 @@
+
+SET LINESIZE 200
+
+SELECT tablespace_name,
+       block_size,
+       extent_management,
+       allocation_type,
+       segment_space_management,
+       status
+FROM   dba_tablespaces
+ORDER BY tablespace_name;
