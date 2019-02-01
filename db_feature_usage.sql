c53713f28efba47f80d902f89469c81fe14d58d8
Sokol Saiti <3602050+sokolsaiti@users.noreply.github.com>
2019-02-01 13:31:53 +0100

    Create db_feature_usage.sql

---
+++ db_feature_usage.sql
@@ -0,0 +1,17 @@
+SET LINESIZE 800
+SET PAGESIZE 1000
+
+COLUMN name  FORMAT A60
+
+
+SELECT u1.name,
+       u1.detected_usages,
+       u1.currently_used,
+       u1.version
+FROM   dba_feature_usage_statistics u1
+WHERE  u1.version = (SELECT MAX(u2.version)
+                     FROM   dba_feature_usage_statistics u2
+                     WHERE  u2.name = u1.name)
+AND    u1.detected_usages > 0
+AND    u1.dbid = (SELECT dbid FROM v$database)
+ORDER BY u1.name;
